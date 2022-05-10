c11bf35ae83ba03906f6ec06c91abcb9654e40d5
Dawid Rusnak <dawid@drcode.pl>
2022-05-10 19:51:55 +0200

    test if ~/.bash_profile exists

--- scripts/_init.sh
+++ scripts/_init.sh
@@ -3,4 +3,6 @@
 SCRIPTS_PATH="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPTS_PATH/_helpers.sh"
 source "$SCRIPTS_PATH/_configure.sh"
-source ~/.bash_profile
+if test -f "~/.bash_profile"; then
+  source ~/.bash_profile
+fi
