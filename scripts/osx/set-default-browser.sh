2d3d0977c6802f48b00ae874db73a770f4bf5121
Dawid Rusnak <dawid@drcode.pl>
2022-05-11 13:07:47 +0200

    fix set default browser

--- scripts/osx/set-default-browser.sh
+++ scripts/osx/set-default-browser.sh
@@ -18,7 +18,7 @@ on run
 		tell application \"System Events\"
 			tell application process \"CoreServicesUIAgent\"
 				tell window 1
-					tell (first button whose name starts with \"use\")
+					tell (first button whose name contains \"Chrome\")
 						perform action \"AXPress\"
 					end tell
 				end tell
@@ -28,4 +28,4 @@ on run
 end run
   "
 }
-set_default_browser kokokokok || set_default_browser canary || set_default_browser chrome || set_default_browser firefox
+set_default_browser kokokokok || set_default_browser canary || set_default_browser chrome
