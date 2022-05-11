7cb564d627d2be3d34bc5ca7800739269f809b7c
Dawid Rusnak <dawid@drcode.pl>
2022-05-11 12:57:55 +0200

    Automatically accept default browser

--- scripts/osx/set-default-browser.sh
+++ scripts/osx/set-default-browser.sh
@@ -8,4 +8,24 @@ header "OSX: Set default browser"
 
 # Set default browser
 brew install defaultbrowser
-defaultbrowser canary || defaultbrowser chrome || defaultbrowser firefox
+
+set_default_browser() {
+  # "name"
+  osascript -e "
+on run
+	do shell script \"defaultbrowser $1\"
+	try
+		tell application \"System Events\"
+			tell application process \"CoreServicesUIAgent\"
+				tell window 1
+					tell (first button whose name starts with \"use\")
+						perform action \"AXPress\"
+					end tell
+				end tell
+			end tell
+		end tell
+	end try
+end run
+  "
+}
+set_default_browser kokokokok || set_default_browser canary || set_default_browser chrome || set_default_browser firefox
