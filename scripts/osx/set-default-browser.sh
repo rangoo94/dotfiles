e75afc645f12971698811e1a436006ea29368a54
Dawid Rusnak <dawid@drcode.pl>
2022-05-11 12:53:07 +0200

    Set default browser correctly

--- scripts/osx/set-default-browser.sh
+++ scripts/osx/set-default-browser.sh
@@ -7,8 +7,5 @@ source "$(dirname "${BASH_SOURCE[0]}")/../_init.sh"
 header "OSX: Set default browser"
 
 # Set default browser
-if [[ -x "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary" ]]; then
-  open -a "Google Chrome Canary" --args --make-default-browser
-elif [[ -x "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" ]]; then
-  open -a "Google Chrome" --args --make-default-browser
-fi
+brew install defaultbrowser
+defaultbrowser canary || defaultbrowser chrome || defaultbrowser firefox
