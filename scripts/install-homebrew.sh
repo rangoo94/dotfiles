a0fb85ca1a401003eb4af5c0539b7ccac91b6a92
Dawid Rusnak <dawid@drcode.pl>
2022-05-27 12:46:53 +0200

    Use local binaries as default (to get these from Homebrew)

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -6,6 +6,9 @@ source "$(dirname "${BASH_SOURCE[0]}")/_init.sh"
 # Show step information
 header "Install Homebrew"
 
+# Use local binaries as preferred
+add_to_bashrc "Local binaries" "export PATH=\"/usr/local/bin:\$PATH\""
+
 # Install Homebrew
 if ! [[ -x "$(command -v brew)" ]]; then
   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
