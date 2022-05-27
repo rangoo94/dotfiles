40f765b90d679faeff4247258640170e47f4de68
Dawid Rusnak <dawid@drcode.pl>
2022-05-27 12:58:45 +0200

    Install Homebrew packages globally

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -9,6 +9,9 @@ header "Install Homebrew"
 # Use local binaries as preferred
 add_to_bashrc "Local binaries" "export PATH=\"/usr/local/bin:\$PATH\""
 
+# Set up proper directory
+add_to_bashrc "Homebrew directory" "export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\""
+
 # Install Homebrew
 if ! [[ -x "$(command -v brew)" ]]; then
   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
