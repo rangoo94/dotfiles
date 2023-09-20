1dd77540fc461d334c80d7474c46f69d14bac3f9
Dawid Rusnak <dawid@drcode.pl>
2023-09-20 15:04:56 +0200

    install Rosetta on Apple M

--- snippets/03-machine.sh
+++ snippets/03-machine.sh
@@ -6,3 +6,9 @@ sudo scutil --set LocalHostName "$(variable_get LOCAL_HOST_NAME)"
 sudo scutil --set ComputerName "$(variable_get COMPUTER_NAME)"
 
 success "Machine configured."
+
+if [[ $(uname -m) == 'arm64' ]]; then
+  header "Install Rosetta"
+  sudo softwareupdate --install-rosetta
+  success "Rosetta installed."
+fi
