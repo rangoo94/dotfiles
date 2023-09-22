1450f03aa557501a2d9eda309a224c479d214534
Dawid Rusnak <dawid@drcode.pl>
2023-09-22 12:19:10 +0200

    add Google Cloud CLI configuration

---
+++ snippets/18-install-gcloud.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if variable_is_true "IS_WORK"; then
+  header "Install Google Cloud CLI"
+
+  brew install --cask google-cloud-sdk
+
+  bashrc_snippet "Google Cloud CLI" "
+    if [ -n \"\$ZSH_VERSION\" ]; then
+     source \"\$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc\"
+     source \"\$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc\"
+    elif [ -n \"\$BASH_VERSION\" ]; then
+      source \"\$(brew --prefix)/share/google-cloud-sdk/path.bash.inc\"
+    fi
+  "
+fi
