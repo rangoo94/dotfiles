3199d2deec48e7d12c1a004c362b790ff71cfaa7
Dawid Rusnak <dawid@drcode.pl>
2023-09-30 14:20:31 +0200

    feat: improve snippet for users' memory/cpu usage

--- snippets/19-snippets.sh
+++ snippets/19-snippets.sh
@@ -4,10 +4,11 @@ header "Installing snippets"
 
 bashrc_snippet "User memory and CPU" "
   used() {
-    (echo 'user cpu% rss';
-      for user in \$(users | tr ' ' '\n' | sort -u); do
-        echo \"\$user \$(ps -U \$user -o rss=,%cpu= | sed s/,/./g | awk '{rss+=\$1; cpu+=\$2} END{print cpu \" \" int(rss/1024) \" MiB\"}')\"
-      done | sort -k3
+    (echo 'user processes cpu% mem';
+      nl=$'\n'
+      for user in \$(ps -e -o user= | sort -u | uniq -u); do
+        ps -U \$user -o rss=,%cpu= | sed s/,/./g | awk -v \"user=\$user\" 'BEGIN{rss=0; cpu=0; processes=0} {rss+=\$1; cpu+=\$2; processes+=1} END{print user \" \" processes \" \" cpu \" \" int(rss/1024) \" MiB\"}'
+      done | sort -k4 -k2 -n
     ) | column -t
   }
 "
