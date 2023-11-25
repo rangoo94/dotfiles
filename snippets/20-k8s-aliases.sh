8a4cc1880856119b58e7fa45f1bd07eb27f60b23
Dawid Rusnak <dawid@drcode.pl>
2023-11-25 22:32:04 +0100

    feat: add Kubernetes helpers

---
+++ snippets/20-k8s-aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if variable_is_true "IS_WORK"; then
+  header "Setup Kubernetes aliases"
+
+  bashrc_snippet "Kubernetes aliases" "
+    k() {kubectl \$@}
+    kc() {kubectx \$@}
+    kctx() {kubectx \$@}
+    kns() {kubens \$@}
+  "
+
+  bashrc_snippet "Telepresence aliases" "
+    tp() {telepresence \$@}
+    tps() {tp status}
+    tpc() {tp connect}
+    tpq() {tp quit}
+  "
+fi
