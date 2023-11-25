2f43773854b58d2460351fb89d0cd23f8047804c
Dawid Rusnak <dawid@drcode.pl>
2023-11-25 23:04:21 +0100

    fix: shell syntax

--- snippets/20-k8s-aliases.sh
+++ snippets/20-k8s-aliases.sh
@@ -4,16 +4,32 @@ if variable_is_true "IS_WORK"; then
   header "Setup Kubernetes aliases"
 
   bashrc_snippet "Kubernetes aliases" "
-    k() {kubectl \$@}
-    kc() {kubectx \$@}
-    kctx() {kubectx \$@}
-    kns() {kubens \$@}
+    k() {
+      kubectl \$@
+    }
+    kc() {
+      kubectx \$@
+    }
+    kctx() {
+      kubectx \$@
+    }
+    kns() {
+      kubens \$@
+    }
   "
 
   bashrc_snippet "Telepresence aliases" "
-    tp() {telepresence \$@}
-    tps() {tp status}
-    tpc() {tp connect}
-    tpq() {tp quit}
+    tp() {
+      telepresence \$@
+    }
+    tps() {
+      tp status
+    }
+    tpc() {
+      tp connect
+    }
+    tpq() {
+      tp quit
+    }
   "
 fi
